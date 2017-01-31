b2ed224a65a360c686b8082d74e0543621938cc3
ady suryadi <ady.limmo@gmail.com>
2017-01-31 21:55:18 +0700

    add env tmdb

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
+
     public function index()
     {
+        $key = env('API_DBTM');
         $client = new Client();
-        $res = $client->request('GET', 'http://api.themoviedb.org/3/movie/top_rated?api_key=842c131a7fb77c69047d99e45188ccb7');
-        $genre = $client->request('GET','https://api.themoviedb.org/3/genre/movie/list?api_key=842c131a7fb77c69047d99e45188ccb7');
+        $res = $client->request('GET', 'http://api.themoviedb.org/3/movie/top_rated?api_key='.$key.'');
+        $genre = $client->request('GET','https://api.themoviedb.org/3/genre/movie/list?api_key='.$key.'');
         $datas = (string) $res->getBody();
         $datagenre = (string) $genre->getBody();
         $data = json_decode($datas, true);
@@ -19,12 +21,12 @@ class MovieController extends Controller
         //dd($genredata);
         return view('film.film', compact('data','genredata'));
     }
-    
     public function lihat($id)
     {
+        $key = env('API_DBTM');
         $client = new Client();
-        $res = $client->request('GET', 'http://api.themoviedb.org/3/movie/'.$id.'?api_key=842c131a7fb77c69047d99e45188ccb7');
-        $videonya = $client->request('GET','http://api.themoviedb.org/3/movie/'.$id.'/videos?api_key=842c131a7fb77c69047d99e45188ccb7');
+        $res = $client->request('GET', 'http://api.themoviedb.org/3/movie/'.$id.'?api_key='.$key.'');
+        $videonya = $client->request('GET','http://api.themoviedb.org/3/movie/'.$id.'/videos?api_key='.$key.'');
         $datas = (string) $res->getBody();
         $datavid = (string) $videonya->getBody();
         $data = json_decode($datas, true);
